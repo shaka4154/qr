b9895a00e55414db36757378aeb1d899957c8ec3
shaka4154 <ahmetkaan4154@gmail.com>
2024-09-15 01:11:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-
 // Blockly yapılandırması
 function initBlockly() {
     Blockly.inject('blocklyDiv', {
         toolbox: `
             <xml xmlns="http://www.w3.org/1999/xhtml">
                 <block type="controls_if"></block>
-                <block type="controls_repeat_ext">
-                    <value name="TIMES">
-                        <shadow type="math_number">
-                            <field name="NUM">10</field>
-                        </shadow>
-                    </value>
-                </block>
+                <block type="controls_repeat_ext"></block>
                 <block type="logic_compare"></block>
-                <block type="math_number">
-                    <field name="NUM">0</field>
-                </block>
-                <!-- Daha fazla blok ekleyebilirsiniz -->
+                <block type="math_number"></block>
+                <!-- Diğer Scratch blokları buraya eklenebilir -->
             </xml>`,
         trashcan: true
     });
@@ -28,5 +19,36 @@ function exportCode() {
     document.getElementById('codeOutput').textContent = code;
 }
 
+// Phaser ile basit bir oyun sahnesi
+function initPhaser() {
+    const config = {
+        type: Phaser.AUTO,
+        width: 600,
+        height: 400,
+        parent: 'gameDiv',
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+    const game = new Phaser.Game(config);
+
+    function preload() {
+        this.load.image('sprite', 'path/to/sprite.png');
+    }
+
+    function create() {
+        this.add.sprite(100, 100, 'sprite');
+    }
+
+    function update() {
+        // Oyun güncellemeleri buraya
+    }
+}
+
 // Başlangıç
-window.onload = initBlockly;
+window.onload = function() {
+    initBlockly();
+    initPhaser();
+};
