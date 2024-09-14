6ac452adf3f087b687f031f57d38c8b39f691b0b
shaka4154 <ahmetkaan4154@gmail.com>
2024-09-15 01:07:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // Blockly yapılandırması
-const workspace = Blockly.inject('blocklyDiv', {
-    toolbox: `
-        <xml xmlns="http://www.w3.org/1999/xhtml">
-            <block type="controls_if"></block>
-            <block type="controls_repeat_ext">
-                <value name="TIMES">
-                    <shadow type="math_number">
-                        <field name="NUM">10</field>
-                    </shadow>
-                </value>
-            </block>
-            <block type="logic_compare"></block>
-            <block type="math_number">
-                <field name="NUM">0</field>
-            </block>
-        </xml>`
-});
+function initBlockly() {
+    Blockly.inject('blocklyDiv', {
+        toolbox: `
+            <xml xmlns="http://www.w3.org/1999/xhtml">
+                <block type="controls_if"></block>
+                <block type="controls_repeat_ext">
+                    <value name="TIMES">
+                        <shadow type="math_number">
+                            <field name="NUM">10</field>
+                        </shadow>
+                    </value>
+                </block>
+                <block type="logic_compare"></block>
+                <block type="math_number">
+                    <field name="NUM">0</field>
+                </block>
+                <!-- Daha fazla blok ekleyebilirsiniz -->
+            </xml>`,
+        trashcan: true
+    });
+}
 
 // Kodu dışa aktarma işlevi
 function exportCode() {
-    const code = Blockly.JavaScript.workspaceToCode(workspace);
+    const code = Blockly.JavaScript.workspaceToCode(Blockly.getMainWorkspace());
     document.getElementById('codeOutput').textContent = code;
 }
+
+// Başlangıç
+window.onload = initBlockly;
